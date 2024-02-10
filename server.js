868535279b85a2258d7b9c5e72e900481d52a2c5
Akash <unocoin@Unocoins-MacBook-Pro.local>
2024-02-10 13:14:43 +0530

    Request / Response

--- server.js
+++ server.js
@@ -3,12 +3,42 @@ const http = require('http');
 
 const server = http.createServer((req,res) => {
     //process.exit();
-    res.setHeader('Content-type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My Fiest Page</title></head>');
-    res.write('<body><h1>Hello from my first Node JS Server</h1></body?');
-    res.write('</html>');
-    res.end();
+    const url = req.url;
+    
+    // if(url === '/'){
+    //     res.write('<html>');
+    //     res.write('<head><title>Enter Message</title></head>');
+    //     res.write('<body><form action="/home" method="POST"><input type="text" name="message"><button>Send</button></form></body>');
+    //     res.write('</html>');
+    //     return res.end();
+    // }
+    // res.setHeader('Content-type', 'text/html');
+    // res.write('<html>');
+    // res.write('<head><title>My Fiest Page</title></head>');
+    // res.write('<body><h1>Hello from my first Node JS Server</h1></body?');
+    // res.write('</html>');
+    // res.end();
+
+    if (url === '/home'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><h1>Welcome Home</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === '/about'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+    }
+    if(url === '/node'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+    }
+    
 });
 
 server.listen(4000);
