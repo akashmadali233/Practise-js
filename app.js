485142e26d08d1b3e3810c3a71781cbc05e78e3b
Akash <unocoin@Unocoins-MacBook-Pro.local>
2024-02-10 18:46:23 +0530

    parsing the request

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+//Redirecting request
+
 const http = require('http');
 const fs = require('fs');
 
@@ -12,9 +15,19 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
+    const body = [];
+    req.on('data', (chunk)=>{
+        console.log(chunk);
+        body.push(chunk);
+    });
+    req.on('end', ()=>{
+        const parsedBy = Buffer.concat(body).toString();
+        const message = parsedBy.split('=')[1];
+        fs.writeFileSync('message.txt', message);
+    });
+
     res.statusCode = 302;
-    res.setHeader('Location', '/');
+    res.setHeader('Location', '/');   //Redirecting request
     return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
