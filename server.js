ae1d557f2aef8a67f7f9574987acd5eb5912ff21
Akash <unocoin@Unocoins-MacBook-Pro.local>
2024-02-10 12:25:31 +0530

    sending responce set header

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req,res) => {
     //process.exit();
-
+    res.setHeader('Content-type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My Fiest Page</title></head>');
     res.write('<body><h1>Hello from my first Node JS Server</h1></body?');
